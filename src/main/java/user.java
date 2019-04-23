4edb3bed59f6523d554c51ace20b2fe52226647a
guozheng <ouguozheng@hotmail.com>
2019-04-23 23:18:16 +0800

    Added application class where the public static void main method will be called and where the springframework will be loaded

--- src/main/java/user.java
+++ src/main/java/user.java
@@ -1,2 +1,14 @@
-public class user {
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class User
+{
+    @RequestMapping("/")
+    String get_users()
+    {
+        return "return stuff here";
+    }
 }
