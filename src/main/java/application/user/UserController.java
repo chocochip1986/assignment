4b57b920b27dcaf27a58c27511ea37ee037156f7
guozheng <ouguozheng@hotmail.com>
2019-04-24 23:46:29 +0800

    Added application.properties

--- src/main/java/application/user/UserController.java
+++ src/main/java/application/user/UserController.java
@@ -4,10 +4,13 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
+//RestController tells Spring to check this class for request mappings.
+//Unlike rails, there seem to not be a central location where routes are defined.
 @RestController
 @EnableAutoConfiguration
 public class UserController
 {
+    //Request mappings associates a function with a path, http params, headers etc.
     @RequestMapping(value = "/users", method = GET)
     public String get_users()
     {
