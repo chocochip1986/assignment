9ab3c5b495c1a6568499622f129f80626591b8e4
Ou Guozheng <ou_guozheng@mom.gov.sg>
2019-04-26 09:20:10 +0800

    Forgot to commit the User Service class

---
+++ src/main/java/application/user/UserService.java
@@ -0,0 +1,28 @@
+package application.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService
+{
+    @Autowired
+    UserDao userDao;
+
+    public List<User> find_all_users()
+    {
+        return this.userDao.findAll();
+    }
+
+    public Optional<User> find_by(Integer employee_id)
+    {
+        return this.userDao.findById(employee_id);
+    }
+
+    public void create(String name, double salary)
+    {
+        
+    }
+}
