9d56f522ffb4c866805c06b08b8bd01ef4566c54
guozheng <ouguozheng@hotmail.com>
2019-04-27 01:08:20 +0800

    Added a bean application_before_start_up_bean to read data from data.csv file before application starts up. Changed the database configuration to create and drop db. Also created a data.csv file with some entries.

---
+++ src/main/java/application/before_application_startup/ApplicationBeforeStartupBean.java
@@ -0,0 +1,13 @@
+package application.before_application_startup;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationBeforeStartupBean implements CommandLineRunner
+{
+
+    public void run(String... args) throws Exception {
+
+    }
+}
