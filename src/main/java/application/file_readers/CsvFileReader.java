f4a38ae4ff429f93652718b42ffcae5a5ee3561c
guozheng <ouguozheng@hotmail.com>
2019-04-27 20:39:36 +0800

    Abstracted the skip header code to a method by itself. Abstracted the reading of all values functionality to a method by itself.

--- src/main/java/application/file_readers/CsvFileReader.java
+++ src/main/java/application/file_readers/CsvFileReader.java
@@ -18,14 +18,16 @@ public class CsvFileReader
     {
         File data_file = get_data_file_from_resource_dir(file_name);
         BufferedReader file_reader = read_file(data_file);
+        ObjectReader reader = create_csv_reader_for_(klass);
+        skip_header(file_reader);
 
-        CsvMapper csv_mapper = new CsvMapper();
-        CsvSchema csv_schema = generate_csv_schema_for(klass, csv_mapper);
+        return read_all_data_from(reader, file_reader);
+    }
 
-        ObjectReader reader = csv_mapper.reader(klass).with(csv_schema);
+    private <T> List<T> read_all_data_from(ObjectReader reader, BufferedReader file_reader)
+    {
         try
         {
-            file_reader.readLine(); //Skip headers
             MappingIterator<T> mappingIterator = reader.readValues(file_reader);
             return mappingIterator.readAll();
         }
@@ -34,6 +36,30 @@ public class CsvFileReader
             System.out.println("Unable to parse values csv file into iterator: "+e.getMessage());
             return null;
         }
+    }
+
+    private void skip_header(BufferedReader file_reader)
+    {
+        try
+        {
+            file_reader.readLine();
+        }
+        catch ( IOException e )
+        {
+            System.out.println("Unable to skip header!");
+        }
+        catch ( Exception e )
+        {
+            System.out.println("Something went wrong when trying to skip header!");
+        }
+    }
+
+    private ObjectReader create_csv_reader_for_(Class klass)
+    {
+        CsvMapper csv_mapper = new CsvMapper();
+        CsvSchema csv_schema = generate_csv_schema_for(klass, csv_mapper);
+        ObjectReader reader = csv_mapper.reader(klass).with(csv_schema);
+        return reader;
     }
 
     private CsvSchema generate_csv_schema_for(Class klass, CsvMapper csv_mapper)
