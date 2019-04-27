d0572f5733aa9f7cfade318ca66c0a3e72d4a9d7
guozheng <ouguozheng@hotmail.com>
2019-04-27 19:10:53 +0800

    Added a bean class that implements the commandlinerunner interface to run just before the spring application starts up. This will help to facilitate the insertion of data into database everytime it starts up

--- src/main/java/application/before_application_startup/ApplicationBeforeStartupBean.java
+++ src/main/java/application/before_application_startup/ApplicationBeforeStartupBean.java
@@ -1,13 +1,18 @@
 package application.before_application_startup;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ApplicationBeforeStartupBean implements CommandLineRunner
 {
+    @Autowired
+    DataSetup dataSetup;
 
     public void run(String... args) throws Exception {
-
+        System.out.println("Loading data into db");
+        dataSetup.setupData();
+        //TODO insert into database
     }
 }
